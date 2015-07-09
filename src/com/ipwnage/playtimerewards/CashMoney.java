124c4e3fe665a47ec36f04f993c6faca9debbb01
Proplex <Proplex@users.noreply.github.com>
2015-07-08 22:22:18 -0400

    add for droo

--- src/com/ipwnage/playtimerewards/CashMoney.java
+++ src/com/ipwnage/playtimerewards/CashMoney.java
@@ -1,13 +1,34 @@
 package com.ipwnage.playtimerewards;
 
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.logging.Logger;
+import net.milkbowl.vault.economy.Economy;
 
 public class CashMoney extends JavaPlugin {
+	private static final Logger log = Logger.getLogger("Minecraft");
+	public static Economy econ = null;
     @Override
     public void onEnable() {
+    	if(!setupEconomy()){
+    		log.severe((String.format("[%s] - Your server doesn't have Vault installed. Disabling plugin.", getDescription().getName())));
+    	}
+    	
     }
 
     @Override
     public void onDisable() {
 }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
 }
