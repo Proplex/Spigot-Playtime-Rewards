bda996bfd27918666e6029be145d293ea8a3f501
Proplex <Proplex@users.noreply.github.com>
2015-07-08 21:18:15 -0400

    Setup environment

---
+++ src/com/ipwnage/playtimerewards/CashMoney.java
@@ -0,0 +1,13 @@
+package com.ipwnage.playtimerewards;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CashMoney extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+}
+}
