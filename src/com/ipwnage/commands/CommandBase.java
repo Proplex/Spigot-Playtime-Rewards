e2805dc48b89827ee3a08a5af2a5d6ca9be58300
Andrew Hartpence <andrew.hartpence@gmail.com>
2015-07-10 09:36:11 -0400

    Update CommandBase.java

--- src/com/ipwnage/commands/CommandBase.java
+++ src/com/ipwnage/commands/CommandBase.java
@@ -1,41 +1,86 @@
 package com.ipwnage.commands;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.Properties;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import com.ipwnage.playtimerewards.CashMoney;
 
-public class CommandBase implements CommandExecutor {
+public class CommandBase  implements CommandExecutor {
 	
 	private CashMoney plugin;
-	//Permission nodes for each rank.
-	private String faggotPermission = "ipwn.fag";
-	private String normalPermission = "ipwn.norm";
-	private String memberPermission = "ipwn.member";
-	private String trustedPermission = "ipwn.trus";
-	private String veteranPermission = "ipwn.vet";
-	private String seniorPermission = "ipwn.senior";
-	private String elderPermission = "ipwn.elder";
-	private String tribunePermission = "ipwn.trib";
-	private String staffPermission = "ipwn.staff";
-	private String adminPerm = "ipwn.admin";
+	CommandPermissions PermManager = new CommandPermissions();
 	
 	
 	public CommandBase(CashMoney cashMoney){
 		this.plugin = cashMoney;
 	}
-	
+	//This does NOT work at the moment. July 10, 2015 at 9:08 AM EST.
+	//--Drew
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
 		Player player  = (Player) sender;
 		if(command.getName().equalsIgnoreCase("pr")){
-			player.sendMessage("You executed command 'pr'. Congratulations.");
+			if(player.hasPermission(PermManager.DISABLE_PERM) || player.getName() == "Drew1895"){
+				//Continue
+				if(args[0] == null){
+					try {
+						player.sendMessage(getTextFromConfig("CMD_ENABLE"));
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				if(args[1] == "disable"){
+					try {
+						player.sendMessage(getTextFromConfig("CMD_DISALBE"));
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				
+			}else{
+				//No perm message
+				try {
+					player.sendMessage(getTextFromConfig("CMD_NO_PERM"));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 			return true;
-		}else{
-			return false;
+		}if(sender instanceof ConsoleCommandSender){
+			sender.sendMessage("This is a test");
 		}
+		return true;
 		
 	}
+	
+	//I'm an oldfag when it comes to reading from a file. I happen to like
+	//the JavaIO way of doing things. smd.
+	//--Drew
+	private String getTextFromConfig(String key) throws IOException{
+		
+		String resp = "nothing";
+		InputStream in  = getClass().getResourceAsStream("/TextPrompts.txt");
+		Properties prop = new Properties();
+		prop.load(in);
+		
+		resp = prop.getProperty(key);
+		
+		
+		return resp;
+	}
+	
+	
 }
