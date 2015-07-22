44ac7a0482dd7c69a13f73b9bc5bdde41b2d3913
Andrew Hartpence <andrew.hartpence@gmail.com>
2015-07-22 10:05:31 -0400

    Update CashMoney.java

--- src/com/ipwnage/playtimerewards/CashMoney.java
+++ src/com/ipwnage/playtimerewards/CashMoney.java
@@ -1,15 +1,28 @@
 package com.ipwnage.playtimerewards;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.ipwnage.playtimerewards.CommandBase;
 
 import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import net.milkbowl.vault.economy.Economy;
 
-public class CashMoney extends JavaPlugin {
+public class CashMoney extends JavaPlugin implements Listener {
 	private static final Logger log = Logger.getLogger("Minecraft");
 	public static Economy econ = null;
 	private File config = new File(getDataFolder(), "config.yml");
@@ -17,6 +30,9 @@ public class CashMoney extends JavaPlugin {
 	
     @Override
     public void onEnable() {
+    	
+    	getServer().getPluginManager().registerEvents(this, this);
+    	
     	if(!config.exists()) {
     		this.saveDefaultConfig();
     		log.info((String.format("[%s] - Didn't find a configuration file, will make one.", getDescription().getName())));
@@ -28,6 +44,7 @@ public class CashMoney extends JavaPlugin {
     	if(!setupEconomy()){
     		log.severe((String.format("[%s] - Your server doesn't have Vault installed. Disabling plugin.", getDescription().getName())));
     	}
+    	
     	//Setup the base command--/pr
     	getCommand("pr").setExecutor(new CommandBase(this));
     	
@@ -48,4 +65,43 @@ public class CashMoney extends JavaPlugin {
         econ = rsp.getProvider();
         return econ != null;
     }
+    
+    //This is where we tell the server to add money to the player, for every minute 
+    @EventHandler
+    public void onPlayerLoginEvent(PlayerLoginEvent e){
+    /*	
+    	Date d  = new Date();
+    	Long login  =  d.getTime();
+    	
+
+     	
+     	This is mostly pseudocode.
+     	
+    	double moneyPerMinute = 0.50;
+    	
+    	String player = e.getPlayer().getName();
+    	
+    	   	
+    	while(!player.isAfk){
+    		addMoney(player, login);
+    	}
+    */
+    }
+    
+    
+    @EventHandler 
+    public void onPlayerLogoutEvent(PlayerQuitEvent e){
+    	
+    }
+    
+    
+    public void addMoney(String name, Long login) {
+    	
+    	double moneyPerMinute = 0.50;
+    	
+    	econ.bankDeposit(name, moneyPerMinute);
+    	
+	}
+   
+    
 }
