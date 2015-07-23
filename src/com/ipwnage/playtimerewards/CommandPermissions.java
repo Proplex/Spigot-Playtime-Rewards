014f52a54dd197d8b81e3bb5708beeaad0541fe0
Andrew Hartpence <andrew.hartpence@gmail.com>
2015-07-23 08:09:59 -0400

    Delete CommandPermissions.java

--- src/com/ipwnage/playtimerewards/CommandPermissions.java
+++
@@ -1,9 +0,0 @@
-package com.ipwnage.playtimerewards;
-
-public class CommandPermissions {
-	
-	public final String DISABLE_PERM = "ipwn.disable";
-	public final String ENABLE_PERM = "ipwn.enable";
-	public final String DONATOR_RATE = "ipwn.donator";
-
-}
