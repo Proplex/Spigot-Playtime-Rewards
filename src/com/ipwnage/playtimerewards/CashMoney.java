074f3de2ec43f6deaf61572d76fd89a8c8ea9b30
Andrew Hartpence <andrew.hartpence@gmail.com>
2015-07-25 09:04:40 -0400

    Add support for AFK and add support for a config
    Changes were untested. They might be broken.

--- src/com/ipwnage/playtimerewards/CashMoney.java
+++ src/com/ipwnage/playtimerewards/CashMoney.java
@@ -21,7 +21,17 @@ import java.util.logging.Logger;
 
 public class CashMoney extends JavaPlugin implements Listener {
     public static Economy econ = null;
-    public static boolean isAfk = false;
+    public Map<Player, Integer> pi = new HashMap<Player, Integer>();
+
+    public int timeout = getConfig().getInt("timeout") / 2;
+    public boolean logConsole  = getConfig().getBoolean("logToConsole");
+    public boolean measeagePlayer = getConfig().getBoolean("messagePlayer");
+    public boolean checkAfk = getConfig().getBoolean("checkForAfk");
+    public int delay = getConfig().getInt("delay");
+    protected double rate = getConfig().getInt("semiCreativeAmountToGive");
+    protected double donatorRate = getConfig().getDouble("donatorSemiCreativeAmountToGive");
+    protected double survivalWorldRate = getConfig().getDouble("survivalAmountToGive");
+    protected double survivalWorldDonatorRate = getConfig().getDouble("donatorSurvivalAmountToGive");
 
     private static final Logger log = Logger.getLogger("Minecraft");
     private File config = new File(getDataFolder(), "config.yml");
@@ -29,18 +39,10 @@ public class CashMoney extends JavaPlugin implements Listener {
     private BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
     private Map<String, Integer> taskID = new HashMap<String, Integer>();
 
-    //The formula for interval is secconds * ticks(Make this 20). I.E: 60 * 20
-    protected long interval = 60 * 20;
-    protected double rate = 0.5;
-    protected double donatorRate = 1.5;
-    protected double survivalWorldRate = 0.1;
-    protected double survivalWorlDonatorRate = 0.2;
 
     @Override
     public void onEnable() {
-
         getServer().getPluginManager().registerEvents(this, this);
-
         if(!config.exists()) {
             this.saveDefaultConfig();
             log.info((String.format("[%s] - Didn't find a configuration file, will make one.", getDescription().getName())));
@@ -56,9 +58,7 @@ public class CashMoney extends JavaPlugin implements Listener {
 
         //Setup the base command--/pr
         getCommand("pr").setExecutor(new CommandBase(this));
-
     }
-
     @Override
     public void onDisable() {
 
@@ -76,60 +76,62 @@ public class CashMoney extends JavaPlugin implements Listener {
         return econ != null;
     }
 
-
-
-
     @EventHandler
     public void onPLayerJoin(final PlayerJoinEvent event){
-        Player p = event.getPlayer();
+        final Player p = event.getPlayer();
+
+        getServer().getScheduler().scheduleSyncRepeatingTask(this, new AFKListener(this, event.getPlayer()), 0, 2*20);
 
         if(p.hasPermission("ipwnage.rate.donator")){
             //Rate for donators.
             final int tid = getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
                 public void run() {
-                    //DEBUG:
-                    //log.info("Giving " + donatorRate + " to " + event.getPlayer().getName());
-
-
+                    if(logConsole){
+                        log.info(String.format("[%s] %s just recieved payment of: %d", getName(), p.getName(), donatorRate));
+                    }
                     //Yes. I know this method is deprecated, but it works.
+                    if(measeagePlayer){
+                        p.sendMessage(ChatColor.DARK_GREEN + String.format("[iPwnAge Rewards] You just recieved %d for playing on the server! Thanks!", donatorRate));
+                    }
                     econ.depositPlayer(event.getPlayer().getName(), donatorRate);
                 }
-            }, 0, interval);
+            }, 0, delay);
             taskID.put(event.getPlayer().getName(), tid);
 
-        }else{     //Ethier you have the permission or you don't. No need for this if/else to go any further.
+        }else{
+        //Ethier you have the permission or you don't. No need for this if/else to go any further.
             //Rate for non-donators.
             final int tid = getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
                 public void run() {
-                    //DEBUG:
-                    //log.info("Giving " + rate + " to " + event.getPlayer().getName());
 
+                    if(logConsole){
+                        log.info(String.format("[%s] %s just recieved payment of: %d", getName(), event.getPlayer(), rate));
+                    }
+
+                    if(measeagePlayer){
+                        p.sendMessage(ChatColor.DARK_GREEN + String.format("[iPwnAge Rewards] You just recieved %d for playing on the server! Thanks!", rate));
+                    }
                     //Yes. I know this method is deprecated, but it works.
                     econ.depositPlayer(event.getPlayer().getName(), rate);
                 }
-            }, 0, interval);
-
+            }, 0, delay);
             taskID.put(event.getPlayer().getName(), tid);
         }
-
     }
-
 
     @EventHandler
-    public void onPlayerMove(final PlayerMoveEvent e) throws InterruptedException {
-        //Store there current x y and z in variables.
-        final int xPos = (int) e.getFrom().getX();
-
-
-        if(yawChange || pitchChange){
-            e.getPlayer().sendMessage(ChatColor.AQUA + "You are not afk.");
+    public void onPlayerMove(PlayerMoveEvent e) {
+        if(e.getFrom().getBlockX()==e.getTo().getBlockX() && e.getFrom().getBlockZ()==e.getTo().getBlockZ()){
+            return;
+        }
+        if(pi.containsKey(e.getPlayer())){
+            pi.remove(e.getPlayer());
+            pi.put(e.getPlayer(), 0);
         }else{
-            e.getPlayer().sendMessage(ChatColor.RED  + "You are afk.");
+            pi.put(e.getPlayer(), 0);
         }
 
-
     }
-
 
     @EventHandler
     public void onPLayerLeave(PlayerQuitEvent e){
@@ -139,12 +141,4 @@ public class CashMoney extends JavaPlugin implements Listener {
             taskID.remove(e.getPlayer().getName());
         }
     }
-
-
-
-    public boolean isAfk(String playerName){
-        return true;
-    }
-
-
 }
