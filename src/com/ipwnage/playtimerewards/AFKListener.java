7692fe30fceca825ee723a3d4715fabb5098a897
Proplex <Proplex@users.noreply.github.com>
2015-07-29 21:57:44 -0400

    Begin work on full rewrite Using per-player event handlers is a bad idea and prone to silently breaking. Let's try something different.

--- src/com/ipwnage/playtimerewards/AFKListener.java
+++ src/com/ipwnage/playtimerewards/AFKListener.java
@@ -1,97 +1,37 @@
 package com.ipwnage.playtimerewards;
 
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-/**
- * Created by andrewhartpence on 7/25/15.
- */
-public class AFKListener implements Runnable{
+
+public class AFKListener {
     CashMoney cashMoney;
-    Player p;
-    int tid;
-    boolean isAfk = false;
+    PlayerData data = new PlayerData();
 
-
-    public AFKListener(CashMoney CashMoney, Player p){
+    public AFKListener(CashMoney CashMoney){
         this.cashMoney  = CashMoney;
-        this.p = p;
-    }
-    public AFKListener(){
-
-    }
-
-
-    @Override
-    public void run() {
-        if (!cashMoney.pi.containsKey(p)) {
-            cashMoney.pi.put(p, 1.0);
-        } else {
-            double value = cashMoney.pi.get(p) + 1;
-            cashMoney.pi.remove(p);
-            cashMoney.pi.put(p, value);
-        }
-        if (cashMoney.pi.containsKey(p)) {
-            double value = cashMoney.pi.get(p);
-            if (value >= cashMoney.timeout) {
-                isAfk = true;
-                if(cashMoney.taskID.containsKey(p.getName())){
-                	p.sendMessage(String.format(ChatColor.DARK_GREEN + "[%s]" + ChatColor.AQUA +  " You are AFK; you are" + ChatColor.RED + " not"+ ChatColor.AQUA+ " receiving money for playing.", cashMoney.serverName));
-                    int tid = cashMoney.taskID.get(p.getPlayer().getName());
-                    cashMoney.getServer().getScheduler().cancelTask(tid);
-                    cashMoney.taskID.remove(p.getName());
-                }
-            }
-
-
-            if (isAfk && value <= 1.0){
-                isAfk = false;
-                if(p.hasPermission("playertime.rate.donator")){
-                    //Rate for donators.
-                    if(cashMoney.debug){
-                        CashMoney.log.info(String.format("Adding %s to the money scheduler", p.getPlayer().getName()));
-                    }
-                    final int tid = cashMoney.getServer().getScheduler().scheduleSyncRepeatingTask(cashMoney, new Runnable() {
-                        public void run() {
-                            if(cashMoney.logConsole){
-                                CashMoney.log.info(String.format("[%s] %s just received payment of: %f",cashMoney.getName(), p.getName(), cashMoney.donatorRate));
-                            }
-                            //Yes. I know this method is deprecated, but it works.
-                            if(cashMoney.measeagePlayer){
-                                p.sendMessage(net.md_5.bungee.api.ChatColor.DARK_GREEN + String.format("[%s] You just received %f for playing on the server! Thanks!",cashMoney.serverName, cashMoney.donatorRate));
-                            }
-                            CashMoney.econ.depositPlayer(p.getName(), cashMoney.donatorRate);
-                        }
-                    }, cashMoney.delay, cashMoney.delay);
-                    cashMoney.taskID.put(p.getName(), tid);
-                    p.sendMessage(String.format(ChatColor.DARK_GREEN + "[%s]" + ChatColor.AQUA +  " You are no longer AFK; you are once again receiving money for playing.",cashMoney.serverName));
-
-                }else{
-                    if(cashMoney.debug){
-                        CashMoney.log.info(String.format("Adding %s to the money scheduler", p.getPlayer().getName()));
-                    }
-                    //Ethier you have the permission or you don't. No need for this if/else to go any further
-                    //Rate for non-donators.
-                    final int tid = cashMoney.getServer().getScheduler().scheduleSyncRepeatingTask(cashMoney, new Runnable() {
-                        @Override
-                        public void run() {
-
-                            if(cashMoney.logConsole){
-                                CashMoney.log.info(String.format("[%s] %s just recieved payment of: %f",cashMoney.getName(), p.getName(), cashMoney.regularRate));
-                            }
-
-                            if(cashMoney.measeagePlayer){
-                                p.sendMessage(net.md_5.bungee.api.ChatColor.DARK_GREEN + String.format("[%s] You just received %f for playing on the server! Thanks!",cashMoney.serverName, cashMoney.regularRate));
-                            }
-                            //Yes. I know this method is deprecated, but it works.
-                            CashMoney.econ.depositPlayer(p.getName(), cashMoney.regularRate);
-                        }
-                    }, cashMoney.delay, cashMoney.delay);
-                    cashMoney.taskID.put(p.getName(), tid);
-                    p.sendMessage(String.format(ChatColor.DARK_GREEN + "[%s]" + ChatColor.AQUA +  " You are no longer AFK; you are once again receiving money for playing.", cashMoney.serverName));
-                }
-
-            }
-        }
+        
+    	for(Player player : Bukkit.getOnlinePlayers()) {
+    		String username = player.getName();
+    		Location location = player.getLocation();
+    		if (data.playerExists(username)) {
+    			if (data.getPlayerLocation(username) == location) {
+    				if (data.getPlayerTimestamp(username) - System.currentTimeMillis() / 1000L > 5) {
+    					data.setAFK(username, true);
+    				}
+    			} else {
+    				data.storePlayerLocation(username, location);
+    				data.setAFK(username, false);
+    			}
+    		} else {
+    			data.storePlayerTimestamp(username, System.currentTimeMillis() / 1000L);
+    			data.storePlayerLocation(username, location);
+    			data.setAFK(username, false);
+    		}
+    	}
     }
 }
---
+++ src/com/ipwnage/playtimerewards/PlayerData.java
@@ -0,0 +1,40 @@
+package com.ipwnage.playtimerewards;
+
+import java.util.HashMap;
+
+import org.bukkit.Location;
+
+public class PlayerData {
+	private HashMap<String, Location> playerlocationdata = new HashMap<String, Location>();
+	private HashMap<String, Long> playertimestampdata = new HashMap<String, Long>();
+	private HashMap<String, Boolean> playerafk = new HashMap<String, Boolean>();
+	
+	public void storePlayerLocation(String username, Location location) {
+		playerlocationdata.put(username, location);
+	}
+	
+	public boolean playerExists(String username) {
+		return playerlocationdata.containsKey(username);
+	}
+	
+	public Location getPlayerLocation(String username) {
+		return playerlocationdata.get(username);
+		
+	}
+	
+	public void storePlayerTimestamp(String username, Long timestamp) {
+		playertimestampdata.put(username, timestamp);
+	}
+	
+	public Long getPlayerTimestamp(String username) {
+		return playertimestampdata.get(username);
+	}
+
+	public void setAFK(String username, Boolean status) {
+		playerafk.put(username, status);
+	}
+	
+	public Boolean isAFK(String username) {
+		return playerafk.get(username);
+	}
+}
